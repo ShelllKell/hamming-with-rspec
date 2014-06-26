c40e24b4e88eb8c58835517ca8551771b4db5a26
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-26 10:19:50 -0600

    This took way longer than it should have

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,59 @@
 class DnaStrandPair
 
-end
+  #we need to match up both arrays to each other
+  #see if any letters do not match up
+  #count that number of mismatching letters
+
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+
+  end
+
+
+  def hamming_distance
+      array1 = @strand_1.split(//)
+      array2 = @strand_2.split(//)
+      columns = 0
+      diff = 0
+
+      while columns < 6
+        if array1[columns] != array2[columns] && array1.length == array2.length
+          diff = diff +1
+        end
+        columns = columns + 1
+      end
+
+      diff
+  end
+
+  end
+
+
+
+
+
+
+#     array1 = @strand_1.split(//)
+#     array2 = @strand_2.split(//)
+#     columns = 0
+#     diff = 0
+#
+#     while columns < 6
+#       if array1[columns] != array2[columns] && array1.length == array2.length
+#         diff = diff + 1
+#       end
+#       columns = columns + 1
+#     end
+#
+#     diff
+#   end
+#
+#
+#
+# end
+
+
+
+
+
